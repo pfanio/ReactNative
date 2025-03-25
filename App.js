6d112f54ed7b0637e02c03d72c2eace629e07940
Paulo Henrique Epifanio de Oliveira <pfanio@gmail.com>
2025-03-25 17:59:05 -0300

    Aula 25/03

--- App.js
+++ App.js
@@ -1,76 +1,28 @@
-import { Button, StyleSheet, Text, TextInput, View, ScrollView } from 'react-native';
-import { useState } from 'react';
+import { StyleSheet, ImageBackground } from 'react-native';
+import { LinearGradient } from 'expo-linear-gradient';
+
+import StartGameScreen from './screens/StartGameScreen';
 
 export default function App() {
-
-  const [item, setItem] = useState('');
-  const [objList, setObjList] = useState([]);
-
-  function addToList() {
-    setObjList((listAtual) => [...listAtual, item]);
-  }
-
-  function removeItem(item) {
-    console.log(item)
-  }
-
   return (
-    <View style={styles.container}>
-      <View style={styles.header}>
-        <TextInput style={styles.input}
-          onChangeText={setItem}
-          value={item}
-          placeholder='Seu objetivo'
-        >
-        </TextInput>
-        <Button title='Adicionar' onPress={addToList} />
-      </View>
-      <View style={styles.list}>
-        <ScrollView>
-          {objList.map((obj) =>
-            <View style={styles.listItem}>
-              <Text style={styles.textItem} key={obj}>{obj}</Text>
-            </View>)}
-        </ScrollView>
-      </View>
-    </View>
+    <LinearGradient colors={['#4e0329', '#ddb52f']} style={styles.rootScreen}>
+      <ImageBackground
+        source={require('./assets/images/background.png')}
+        resizeMode="cover"
+        style={styles.rootScreen}
+        imageStyle={styles.backgroundImage}
+      >
+        <StartGameScreen />
+      </ImageBackground>
+    </LinearGradient>
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
+  rootScreen: {
     flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    paddingTop: 50,
-    paddingHorizontal: 16
   },
-  header: {
-    flex: 1,
-    flexDirection: 'row',
-    justifyContent: 'space-between',
-    alignItems: 'center',
-    borderBottomColor: '#ccc',
-    borderBottomWidth: 1,
-    marginBottom: 24
-  },
-  input: {
-    padding: 8,
-    borderWidth: 1,
-    borderColor: '#ccc',
-    width: '70%',
-    marginRight: 8
-  },
-  list: {
-    flex: 5,
-  },
-  listItem: {
-    backgroundColor: '#5e0acc',
-    margin: 8,
-    padding: 8,
-    borderRadius: 6
-  },
-  textItem: {
-    color: 'white'
+  backgroundImage: {
+    opacity: 0.15
   }
 });
